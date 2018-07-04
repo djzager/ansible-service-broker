bb1804217fc6b1f19db724c93c8a152104a7ab8e
PhilipGough <pgough@redhat.com>
2018-07-04 11:36:04 +0100

    Add missing namespace flag to debug script

--- scripts/prep_debug_env.sh
+++ scripts/prep_debug_env.sh
@@ -12,9 +12,9 @@ oc env dc/${DEPLOYMENT_NAME} DEBUG_ENABLED=True --overwrite=true -n ${PROJECT}
 oc rollout resume dc/${DEPLOYMENT_NAME} -n ${PROJECT}
 oc patch svc ${DEPLOYMENT_NAME} --patch='{"spec":{"ports":[{"name":"debug", "port":'${DEBUG_PORT}',"targetPort":'${DEBUG_PORT}}']}}' -n ${PROJECT}
 sleep 5
-POD_NAME="$(oc get po -o jsonpath='{.items[?(@.metadata.labels.service=="'${DEPLOYMENT_NAME}'")].metadata.name}')"
-until oc get po ${POD_NAME} -o jsonpath='{.status.containerStatuses[0].ready}' | grep "true" >/dev/null 2>&1 ; do sleep 1; done
 
+POD_NAME="$(oc get po -o jsonpath='{.items[?(@.metadata.labels.service=="'${DEPLOYMENT_NAME}'")].metadata.name}' -n ${PROJECT})"
+until oc get po ${POD_NAME} -o jsonpath='{.status.containerStatuses[0].ready}' -n ${PROJECT} | grep "true" >/dev/null 2>&1 ; do sleep 1; done
 oc port-forward ${POD_NAME} ${DEBUG_PORT} -n ${PROJECT}
 
 echo "To revert these changes, run ./scripts/clean_debug_env.sh ${PROJECT} ${DEPLOYMENT_NAME}"
